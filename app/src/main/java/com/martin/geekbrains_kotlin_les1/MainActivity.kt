fce9b36e15426dde2165dd6e0fc5b2a9d5e79b3e
martin <vasiljevs.martins@gmail.com>
2019-02-26 13:08:53 +0100

    init

---
+++ app/src/main/java/com/martin/geekbrains_kotlin_les1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.martin.geekbrains_kotlin_les1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btn: Button = findViewById(R.id.btn)
+        btn.setOnClickListener(View.OnClickListener { v ->
+            Toast.makeText(this, "Button pressed", Toast.LENGTH_LONG).show();
+        })
+    }
+}
